62b66361fc74215749fd82d7b8f4b0dd373428ac
Prakhar Verma <prakharverma3march@gmail.com>
2024-05-23 23:20:41 +0530

    removed mlq, f_b

--- MLQScheduler.h
+++ MLQScheduler.h
@@ -1,8 +1,5 @@
 #ifndef MLQ_SCHEDULER_H
 #define MLQ_SCHEDULER_H
-#include <queue>
-#include <vector>
-#include <string>
 
 #include "SchedulingAlgorithm.h"
 #include <iostream>
@@ -14,25 +11,7 @@ public:
     void F_B_Scheduling() override;
     void MLQ_Scheduling() override;
 
-    void addProcess(Process p) {
-        if (p.priority >= 0 && p.priority < queues.size()) {
-            queues[p.priority].push(p);
-        } else {
-            std::cerr << "Invalid priority for process " << p.name << std::endl;
-        }
-    }
-    
-private:
-    std::vector<std::queue<Process>> queues;
-
 };
-class Process {
-public:
-    int pid;
-    int priority;
-    std::string name;
 
-    Process(int p, int pr, std::string n) : pid(p), priority(pr), name(n) {}
-};
 
 #endif // MLQ_SCHEDULER_H
